079f36480519f29fadf068a2813749ac6b7c4c53
Deepam <mail4deepamgoel@gmail.com>
2020-08-14 17:11:12 +0530

    simplify ui and code refactor

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 var turn;
 var msgLabel;
-var startButton;
 var resetButton;
 var gridButtons;
-var gridEntry;
+var gridMatrix;
 
 window.addEventListener('load', init);
 
 function init() {
     msgLabel = document.getElementById('msgLabel');
-    startButton = document.getElementById('startButton');
     resetButton = document.getElementById('resetButton');
     gridButtons = document.getElementsByClassName('gridButton');
 
@@ -18,131 +16,134 @@ function init() {
 }
 
 function initEvents() {
-    startButton.addEventListener('click', onStartClick);
     resetButton.addEventListener('click', onResetClick);
     for (let button of gridButtons)
         button.addEventListener('click', onGridButtonClick);
 }
 
-function onStartClick() {
-    turn++;
-    updateMsgLabel();
-    setGridDisabled(false);
-    startButton.disabled = true;
-    resetButton.disabled = false;
-}
-
 function onResetClick() {
-    turn = 0;
-    resetGridLabel();
-    resetGridEntry();
-    updateMsgLabel();
-    setGridDisabled(true);
-    startButton.disabled = false;
-    resetButton.disabled = true;
+    turn = 1;
+    resetGridButtons();
+    resetGridMatrix();
+    updateMsgLabel(null);
+    setGridDisabled(false)
 }
 
 function onGridButtonClick(event) {
     let button = event.target;
 
-    button.disabled = true;
-    updateGridLabel(button);
-    updateGridEntry(parseInt(button.id));
+    updateGridButton(button);
+    updateGridMatrix(parseInt(button.id));
 
-    let isWin = checkWin(parseInt(button.id))
+    let isWin = checkWin(parseInt(button.id));
     if (isWin) {
-        highlightWin(isWin['ids']);
-        updateMsgLabel(true);
         setGridDisabled(true);
-        startButton.disabled = true;
-        resetButton.disabled = false;
+        highlightButtons(isWin['ids']);
+        updateMsgLabel(isWin['msg']);
         return;
     }
 
     turn++;
-    updateMsgLabel();
+    updateMsgLabel(null);
 }
 
 function checkWin(id) {
     if (turn < 5)
         return;
 
-    var r = Math.floor(id / 10);
-    var c = id % 10;
-    var value = gridEntry[r][c];
+    let r = Math.floor(id / 10);
+    let c = id % 10;
+    let value = gridMatrix[r][c];
+
+    let winner = turn % 2 == 1 ? 'X' : 'O';
+    let msg = '<b>PLAYER ' + winner + ' WINS</b>';
 
     // vertical
-    if (gridEntry[0][c] == value && gridEntry[1][c] == value && gridEntry[2][c] == value) {
-        return { 'ids': ['0' + c, '1' + c, '2' + c] };
+    if (gridMatrix[0][c] == value && gridMatrix[1][c] == value && gridMatrix[2][c] == value) {
+        return {
+            'ids': ['0' + c, '1' + c, '2' + c],
+            'msg': msg
+        };
     }
 
     // horizontal
-    if (gridEntry[r][0] == value && gridEntry[r][1] == value && gridEntry[r][2] == value) {
-        return { 'ids': [r + '0', r + '1', r + '2'] };
+    if (gridMatrix[r][0] == value && gridMatrix[r][1] == value && gridMatrix[r][2] == value) {
+        return {
+            'ids': [r + '0', r + '1', r + '2'],
+            'msg': msg
+        };
     }
 
     // diagonal
-    if (id == 10 || id == 12 || id == 01 || id == 21)
-        return;
-    if (gridEntry[0][0] == value && gridEntry[1][1] == value && gridEntry[2][2] == value) {
-        return { 'ids': ['00', '11', '22'] };
+    if (gridMatrix[0][0] == value && gridMatrix[1][1] == value && gridMatrix[2][2] == value) {
+        return {
+            'ids': ['00', '11', '22'],
+            'msg': msg
+        };
     }
-    if (gridEntry[0][2] == value && gridEntry[1][1] == value && gridEntry[2][0] == value) {
-        return { 'ids': ['02', '11', '20'] };
+    if (gridMatrix[0][2] == value && gridMatrix[1][1] == value && gridMatrix[2][0] == value) {
+        return {
+            'ids': ['02', '11', '20'],
+            'msg': msg
+        };
+    }
+
+    if (turn == 9) {
+        return {
+            'ids': [
+                '00', '01', '02',
+                '10', '11', '12',
+                '20', '21', '22'
+            ],
+            'msg': '<b>DRAW</b>'
+        };
     }
 }
 
-function highlightWin(ids) {
-    ids.forEach(id => {
-        document.getElementById(id).style.borderColor = 'black';
-    });
-}
-
-function updateMsgLabel(isWin = false) {
-    if (isWin) {
-        winner = turn % 2 == 1 ? 'A' : 'B';
-        msg = '<b>PLAYER ' + winner + ' WINS</b>';
-    } else {
-        if (turn == 0)
-            msg = '';
-        else if (turn == 10)
-            msg = '<b>DRAW</b>';
-        else if (turn % 2 == 1)
-            msg = 'Player A\'s turn';
-        else
-            msg = 'Player B\'s turn';
-    }
-    msgLabel.innerHTML = msg;
-}
-
-function resetGridLabel() {
+function resetGridButtons() {
     for (let button of gridButtons) {
+        button.disabled = false;
         button.innerText = '';
         button.style.background = 'powderblue';
         button.style.borderColor = 'unset';
     }
 }
 
-function updateGridLabel(button) {
-    if (turn % 2 == 1)
-        button.innerText = 'O';
-    else
-        button.innerText = 'X';
-}
-
-function resetGridEntry(button) {
-    gridEntry = [
+function resetGridMatrix(button) {
+    gridMatrix = [
         [null, null, null],
         [null, null, null],
         [null, null, null],
     ];
 }
 
-function updateGridEntry(id) {
-    gridEntry[Math.floor(id / 10)][id % 10] = turn % 2;
+function updateMsgLabel(msg) {
+    if (msg == null) {
+        let player = turn % 2 == 1 ? 'X' : 'O';
+        msg = 'Player ' + player + '\'s turn';
+    }
+    msgLabel.innerHTML = msg;
+}
+
+function updateGridButton(button) {
+    button.disabled = true;
+    if (turn % 2 == 1)
+        button.innerText = 'X';
+    else
+        button.innerText = 'O';
+}
+
+function updateGridMatrix(id) {
+    gridMatrix[Math.floor(id / 10)][id % 10] = turn % 2;
 }
 
 function setGridDisabled(isDisabled) {
     for (let button of gridButtons)
         button.disabled = isDisabled;
+}
+
+function highlightButtons(ids) {
+    ids.forEach(id => {
+        document.getElementById(id).style.borderColor = 'black';
+    });
 }
